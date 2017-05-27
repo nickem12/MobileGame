ca51549b1d08d8e1b8221acbe6a2755170c4084a
Benjamin08 <RFLXcolby@gmail.com>
2017-05-27 12:00:36 -0300

    Gyro and Some menus

---
+++ MobileGame/Assets/Colby/Scripts/GyroControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroControl : MonoBehaviour {
+    public bool gyroEnabled;
+    private Gyroscope gyro;
+    private GameObject cubeParent;
+    private Quaternion rotation;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        cubeParent = new GameObject("cube parent");
+        cubeParent.transform.position = transform.position;
+        transform.SetParent(cubeParent.transform);
+
+
+        gyroEnabled = EnableGyro();
+
+        Debug.Log(gyroEnabled);
+    }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+
+            rotation = new Quaternion(0, 0, 1, 0);
+
+            return true;
+        }
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gyroEnabled)
+        {
+            transform.localRotation = gyro.attitude * rotation;
+        }
+    }
+}
---
+++ MobileGame/Assets/Colby/Scripts/UIButtonFunctions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtonFunctions : MonoBehaviour {
+
+    private GameObject theCanvas;
+    private GameObject theCamera;
+    public float cameraMovementScalar;
+
+	// Use this for initialization
+	void Start () {
+        theCanvas = GameObject.FindGameObjectWithTag("Canvas");
+        theCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(theCamera.transform.position);
+	}
+
+    public void NewGame()
+    {
+        theCanvas.GetComponent<Canvas>().enabled = false;
+        Vector3 startingPos = theCamera.transform.position;
+        Vector3 endPos = new Vector3(0, 23, -24);
+        theCamera.transform.position = Vector3.Lerp(startingPos, endPos, Time.deltaTime * cameraMovementScalar);
+    }
+}
